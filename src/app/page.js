cd89c31d24e9449cc628eabdf9f4675e4b1b9422
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-01-11 18:51:44 +0100

    button animation

--- src/app/page.js
+++ src/app/page.js
@@ -102,7 +102,7 @@ export default function Home() {
 								width='24'
 								height='24'
 								xmlns='http://www.w3.org/2000/svg'>
-								<g fill='none' fill-rule='evenodd'>
+								<g fill='none' fillRule='evenodd'>
 									<circle
 										fill='#FF7979'
 										cx='12'
